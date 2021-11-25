c368f0fad6660afe812ece345cd160960bae7494
kimminse3380 <mss2214@naver.com>
2021-11-25 17:41:11 +0900

    Todolist, TodoInsert and TodoList

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
 import React from "react";
-import { SafeAreaView, StyleSheet, Text} from "react-native";
+import { 
+SafeAreaView, 
+StyleSheet, 
+TextInput,
+View, 
+Text,
+ScrollView
+} from "react-native";
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return(
+  return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.appTitle}>Hello Todolist</Text>
+      <View style={styles.card}>
+        <TodoInsert />
+        <TodoList />
+      </View>
     </SafeAreaView>
   );
 };
 
+
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
+    backgroundColor: '#3143e8',
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8',
+  },
+  card: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent:'center',
+    flex: 1,
+    borderTopLeftRadius: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft:20,
   },
 });
 
